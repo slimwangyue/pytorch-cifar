d6ee1c833d1828e29876cc05589eb8c4eb9b99a3
slimwangyue <slimwangyue@gmail.com>
2019-05-02 02:24:48 -0500

    Customized backward

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ import os
 import argparse
 
 from models import *
-from utils import progress_bar
+# from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -41,16 +42,16 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
+net = VGG('VGG19')
 # net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
@@ -61,7 +62,7 @@ print('==> Building model..')
 # net = DPN92()
 # net = ShuffleNetG2()
 # net = SENet18()
-net = ShuffleNetV2(1)
+# net = ShuffleNetV2(1)
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -99,8 +100,9 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx % 500 == 0:
+            print('Loss: %.3f | Acc: %.3f%% (%d/%d)' % (
+            train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
 
 def test(epoch):
     global best_acc
@@ -118,9 +120,7 @@ def test(epoch):
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            print('Loss: %.3f | Acc: %.3f%% (%d/%d)' % (test_loss / (batch_idx + 1), 100. * correct / total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
--- models/vgg.py
+++ models/vgg.py
@@ -1,6 +1,7 @@
 '''VGG11/13/16/19 in Pytorch.'''
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 
 cfg = {
@@ -30,7 +31,24 @@ class VGG(nn.Module):
             if x == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                tmp_conv2d = nn.Conv2d(in_channels, x, kernel_size=3, padding=1, bias=False)
+
+                def hookFunc(conv2d_obj, saved_variables, grads):
+                    x_grad = None
+                    w_grad = None
+                    bias_grad = None
+                    x_in, w, b = saved_variables
+                    if x_in is not None and w is not None:
+                        x_grad = torch.nn.grad.conv2d_input(x_in.shape, w, grads[0], stride=conv2d_obj.stride, padding=conv2d_obj.padding)
+                    if x_in is not None and w is not None:
+                        w_grad = torch.nn.grad.conv2d_weight(x_in, w.shape, grads[0], stride=conv2d_obj.stride, padding=conv2d_obj.padding)
+                    if b is not None:
+                        bias_grad = torch.ones(b.shape, device=torch.device('cuda:0')) * torch.sum(grads[0], dim=(0, 2, 3))
+                    return x_grad, w_grad, bias_grad
+
+                tmp_conv2d.register_backward_hook(hookFunc)
+
+                layers += [tmp_conv2d,
                            nn.BatchNorm2d(x),
                            nn.ReLU(inplace=True)]
                 in_channels = x
@@ -40,7 +58,7 @@ class VGG(nn.Module):
 
 def test():
     net = VGG('VGG11')
-    x = torch.randn(2,3,32,32)
+    x = torch.randn(2, 3, 32, 32)
     y = net(x)
     print(y.size())
 
